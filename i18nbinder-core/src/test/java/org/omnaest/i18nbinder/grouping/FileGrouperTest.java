4e9d93e0de441f8362e7f007aa64db04cfe86f9f
plathub <plathine@gmail.com>
2017-06-19 16:57:43 +0200

    fixed I18nFacadeTest to run zzz_testMockingTheFacade() as last @Test with JUnit 4.12 and @FixMethodOrder because it has broken the other tests after the call of "I18nFacade.use"

--- i18nbinder-core/src/test/java/org/omnaest/i18nbinder/grouping/FileGrouperTest.java
+++ i18nbinder-core/src/test/java/org/omnaest/i18nbinder/grouping/FileGrouperTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 Danny Kunz
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,26 +25,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class FileGrouperTest
 {
-  
   private String basePath;
-  
+
   @Before
   public void setUp() throws Exception
   {
 	  this.basePath = (System.getProperty("os.name").toLowerCase().contains("win")?"C:\\temp\\":"/tmp/");
   }
-  
+
   @Test
   public void testDetermineFileGroupIdentifierToFileGroupMap()
   {
-    //
     List<File> fileList = new ArrayList<File>();
     fileList.add( new File( basePath + "admin_de_DE.properties" ) );
     fileList.add( new File( basePath + "admin_en_US.properties" ) );
@@ -52,8 +49,7 @@ public class FileGrouperTest
     fileList.add( new File( basePath + "article_en_US.properties" ) );
     fileList.add( new File( basePath + "article_de.properties" ) );
     fileList.add( new File( basePath + "article_en.properties" ) );
-    
-    //
+
     FileGrouper fileGrouper = new FileGrouper();
     try
     {
@@ -67,32 +63,25 @@ public class FileGrouperTest
       e.printStackTrace();
       Assert.fail();
     }
-    
-    //
+
     Map<String, FileGroup> fileGroupIdentifierToFileGroupMap = fileGrouper.determineFileGroupIdentifierToFileGroupMap();
-    
-    //
+
     assertEquals( 2, fileGroupIdentifierToFileGroupMap.size() );
     Set<String> fileGroupIdentifierSet = fileGroupIdentifierToFileGroupMap.keySet();
     assertTrue( fileGroupIdentifierSet.contains( basePath+"admin_{locale}.properties" ) );
     assertTrue( fileGroupIdentifierSet.contains( basePath+"article_{locale}.properties" ) );
-    
-    //
+
     {
-      //
       FileGroup fileGroup = fileGroupIdentifierToFileGroupMap.get( basePath+"admin_{locale}.properties" );
-      
-      //
+
       Map<String, File> groupTokenToFileMap = fileGroup.getGroupTokenToFileMap();
       assertEquals( 2, groupTokenToFileMap.size() );
       assertTrue( groupTokenToFileMap.containsKey( "de_DE" ) );
       assertTrue( groupTokenToFileMap.containsKey( "en_US" ) );
     }
     {
-      //
       FileGroup fileGroup = fileGroupIdentifierToFileGroupMap.get( basePath+"article_{locale}.properties" );
-      
-      //
+
       Map<String, File> groupTokenToFileMap = fileGroup.getGroupTokenToFileMap();
       assertEquals( 4, groupTokenToFileMap.size() );
       assertTrue( groupTokenToFileMap.containsKey( "de_DE" ) );
@@ -101,5 +90,5 @@ public class FileGrouperTest
       assertTrue( groupTokenToFileMap.containsKey( "en" ) );
     }
   }
-  
+
 }
